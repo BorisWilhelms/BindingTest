5af772c15e83c975dae1809a2433f89aef33a2dc
Boris Wilhelms <mail@wille-zone.de>
2018-02-18 16:09:39 +0100

    Adds Newtonsoft Json 11

--- BindingTest/GetPost.cs
+++ BindingTest/GetPost.cs
@@ -1,10 +1,16 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Description;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Host.Config;
 using Newtonsoft.Json;
+using System;
+using System.IO;
 using System.Net.Http;
+using System.Reflection;
+using System.Runtime.Loader;
 using System.Threading.Tasks;
 
 namespace BindingTest
@@ -14,13 +20,31 @@ namespace BindingTest
         private static HttpClient _client = new HttpClient();
 
         [FunctionName("GetPost")]
-        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, [DummyBinding]TraceWriter log)
         {
             var rawPost = await _client.GetStringAsync("https://jsonplaceholder.typicode.com/posts/1");
             var post = JsonConvert.DeserializeObject<Post>(rawPost);
             return new OkObjectResult(post.Body);
         }
     }
+
+    public class asd : IExtensionConfigProvider
+    {
+        public void Initialize(ExtensionConfigContext context)
+        {
+            AssemblyLoadContext.Default.Resolving += Default_Resolving;
+        }
+
+        private Assembly Default_Resolving(AssemblyLoadContext arg1, AssemblyName arg2)
+        {
+            var path = new FileInfo(GetType().Assembly.Location).Directory.FullName;
+
+            return Assembly.LoadFile(Path.Combine(path, "Newtonsoft.Json.dll"));
+        }
+    }
+
+    [Binding]
+    public class DummyBindingAttribute : Attribute { }
 
     public class Post
     {
